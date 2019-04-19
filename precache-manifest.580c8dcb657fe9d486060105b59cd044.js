cd65c8d3bde7cb957df54a2087e237f4b8eb04f2
Riley Davis <riguyrocks01@gmail.com>
2019-04-19 09:42:43 -0600

    Updates

--- precache-manifest.580c8dcb657fe9d486060105b59cd044.js
+++ precache-manifest.580c8dcb657fe9d486060105b59cd044.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React-E-Commerce/static/js/runtime~main.79f67b59.js"
   },
   {
-    "revision": "b7c21851dd48b1302f6d",
-    "url": "/React-E-Commerce/static/js/main.2c072040.chunk.js"
+    "revision": "71b57e3753750e558156",
+    "url": "/React-E-Commerce/static/js/main.81f9fadd.chunk.js"
   },
   {
     "revision": "2a9ad2334db50d2a49a7",
     "url": "/React-E-Commerce/static/js/2.2550dc1f.chunk.js"
   },
   {
-    "revision": "b7c21851dd48b1302f6d",
+    "revision": "71b57e3753750e558156",
     "url": "/React-E-Commerce/static/css/main.4e29f173.chunk.css"
   },
   {
-    "revision": "6498bc7de736003cf31140cb553a46de",
+    "revision": "9ff090e260fea752ba31421447315079",
     "url": "/React-E-Commerce/index.html"
   }
 ];
